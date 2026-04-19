5b731cb37e66ff92e2ec1af1bcb87be7ca692960
YitengGuo <gyt@stu.hist.edu.cn>
2026-04-19 17:35:38 +0800

    Add configs/_base_/datasets/ruod_detection.py

---
+++ configs/_base_/datasets/ruod_detection.py
@@ -0,0 +1,64 @@
+# dataset settings
+dataset_type = 'CocoDataset'
+classes = ('holothurian', 'echinus', 'scallop', 'starfish', 'fish',
+           'corals', 'diver', 'cuttlefish', 'turtle', 'jellyfish')
+data_root = 'data/RUOD/'
+
+img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotations', with_bbox=True),
+    dict(
+        type='Resize',
+        img_scale=[
+            (800, 600),  # 4:3 比例，适用于较低分辨率场景，常见的传统图像比例
+            (1280, 960),  # 4:3 比例，较高分辨率，适合经典的物体检测任务
+            (1333, 800),  # 接近 1.66:1 的比例，较为常见的宽高比，适合宽屏图像
+            (1920, 1080),  # 16:9 比例，现代显示器和视频的标准比例，适合多种场景/*
+       ],
+        multiscale_mode='value',  # 随机选择具体尺寸
+        keep_ratio=True  # 保持长宽比
+    ),
+    dict(type='RandomFlip', flip_ratio=0.5),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='Pad', size_divisor=32),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels'])
+]
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(
+        type='MultiScaleFlipAug',
+        img_scale=(1333, 800),
+        flip=False,
+        transforms=[
+            dict(type='Resize', keep_ratio=True),
+            dict(type='RandomFlip'),
+            dict(type='Normalize', **img_norm_cfg),
+            dict(type='Pad', size_divisor=32),
+            dict(type='ImageToTensor', keys=['img']),
+            dict(type='Collect', keys=['img']),
+        ])
+]
+data = dict(
+    samples_per_gpu=2,
+    workers_per_gpu=4,
+    train=dict(
+        type=dataset_type,
+        ann_file=data_root + 'RUOD_ANN/instances_train.json',
+        img_prefix=data_root + 'RUOD_pic/train/',
+        pipeline=train_pipeline),
+    val=dict(
+        type=dataset_type,
+        ann_file=data_root + 'RUOD_ANN/instances_val.json',
+        img_prefix=data_root + 'RUOD_pic/val/',
+        pipeline=test_pipeline),
+    test=dict(
+        type=dataset_type,
+        ann_file=data_root + 'RUOD_ANN/instances_test.json',
+        img_prefix=data_root + 'RUOD_pic/test/',
+        pipeline=test_pipeline)
+)
+
+evaluation = dict(interval=1, metric='bbox')
